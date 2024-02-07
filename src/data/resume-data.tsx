cb55e2f07f199545197f38a2bc66a4fa2f3b818b
Zach <95212409+starlequin@users.noreply.github.com>
2024-02-07 20:42:30 +0200

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,134 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Webster Mbangani",
+  initials: "WM",
+  location: "Harare, Zimbabwe, CAT",
+  locationLink: "",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Software engineer focused on building products with extra attention to detail.",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "A dependable worker who has operated in high-stress environments and is capable of keeping composure. Has amassed a wealth of experience, expert technical and social skills that I am passionately seeking to leverage to add value to the organization. Core competencies include excellent interpersonal communication with great networking skills. Handles tasks with accuracy and efficiency. Very good at delivering results and meeting targets. Currently, I work mostly with C#, HTML5/CSS3 and JavaScript. I have over 4 years of experience working with companies and people around the world.",
+  avatarUrl: "https://imgur.com/a/H2VAMW3",
+  personalWebsiteUrl: "",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "mirronai@gmail.com",
+    tel: "+263779050002",
     social: [
       {
-        name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
-        icon: GitHubIcon,
-      },
-      {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/websm/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
       },
     ],
   },
-  education: [
-    {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
-    },
-  ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "Oanob Private Hospital",
+      link: "https://www.facebook.com/groups/333159813972672/",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      title: "IT Technician",
+      logo: ,
+      start: "01/2022",
+      end: "01/2024",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        "Administration of Trifour’s TriMed Health Information System. Led teams to install on-site equipment then maintained and monitored them. Managed networks, servers and digital inventory and information. Data sorting. Set up security measures for data protection. Headed teams for stock auditing. Trained groups of employees in usage of company technology. Mediation and assistance between Trifour and the hospital during larger scale software breakdowns.",
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
+      company: "Vertical Momentum",
+      link: "https://www.facebook.com/verticalmomentum/",
       badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      title: "Technical Product Manager",
+      logo: ,
+      start: "08/2019",
+      end: "07/2021",
+      description:
+        "Management of servers, digital inventory and information. Acquisition and installation of technological assets. Website design and development and data sorting. Maintenance of equipment and assets. Set up and monitoring of systems during events. Managed databases. Assisted employees in operation of equipment.",
     },
   ],
   skills: [
+    "C#",
+    "Unity",
+    "HTML5",
+    "CSS3",
     "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "C",
+    "C++"
+    "Bash"
+    "Expert Critical Thinking"
+    "Adept at Hardware And Software Maintenance"
+    "Fast Learner"
+    "Meticulous Time Keeping"
+    "Excellent Organization",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Point and Click Adventure Game Framework",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "C#",
+        "Unity",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Designed and built the framework for a mouse and keyboard controlled game in Unity using C#, with options for branching dialogue choices and paths, UI and pictures drawn and manipulated within the engine, robust saving and automatic saving system, adjustable fonts, volume and brightness controls and adapted that framework to be usable by persons with less expertise through writing in a txt file with simple commands.",
+      logo: ,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "itch.io",
+        href: "https://starlequins.itch.io",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Vertical Momentum Website",
+      techStack: ["HTML5", "CSS3"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "Created a website using HTML5 and CSS3 with both desktop and mobile versions and a data collection system for questions, bookings and other information.",
+      logo: ,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "",
+        href: "",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "Add-On Development",
+      techStack: ["JavaScript"],
       description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+        "Creating add-ons in JavaScript for an engine to give users more control and functionality over the engines capabilities.",
+      logo: ,
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "",
+        href: "",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "POS Software",
+      techStack: ["HTML5"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+        "Wrote a simple program that allows taking of orders, processing of payments, printing of receipts and invoices, tracking of inventory and management of employees in HTML5.",
+      logo: ,
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "",
+        href: "",
       },
     },
+    },
+  ],
+  education: [
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
+      school: "ALX Africa",
+      degree: "Diploma in Software Engineering",
+      start: "02/2023",
+      end: "02/2024",
     },
     {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
+      school: "Gateway High School",
+      degree: "Advanced Level Sciences (2 Bs, 1 C) and IGCSE (5 As and 5Bs)",
+      start: "01/2016",
+      end: "11/2021",
     },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+  ],
+  hobbies: [
+    "Illustration",
+    "Reading and writing literature",
+    "Playing/Developing video games",
+    "Listening to and composing music",
+    "Playing tabletop roleplaying games",
   ],
 } as const;
